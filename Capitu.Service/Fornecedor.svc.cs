89993ce09687e5719f6ed4f00660e11b93bcde9e
gpalazon <gustavo.palazon@gmail.com>
2015-03-17 17:36:48 -0300

    17032015 Fornecdor

---
+++ Capitu.Service/Fornecedor.svc.cs
@@ -0,0 +1,46 @@
+﻿using Capitu.BE;
+using Capitu.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Capitu.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Fornecedor" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Fornecedor.svc or Fornecedor.svc.cs at the Solution Explorer and start debugging.
+    public class Fornecedor : IFornecedor
+    {
+        public void DoWork()
+        {
+        }
+
+        public List<FornecedorDTO> GetFornecedores()
+        {
+            List<FornecedorDTO> ret = new List<FornecedorDTO>();
+            FornecedorBLL fornBll = new FornecedorBLL();
+            foreach (FornecedorBE f in fornBll.GetFornecedores(0, string.Empty))
+            {
+                ret.Add(new FornecedorDTO()
+                {
+                    Altura = f.Altura,
+                    Endereco = f.Endereco,
+                    Etnia = f.Etnia.DsEtnia,
+                    GeoLat = (Double)f.Latitude,
+                    GeoLong = (Double)f.Longitude,
+                    Id = f.Id,
+                    Idade = f.Idade,
+                    Imagem = f.ImgPerfilUrl,
+                    Nome = f.Nome,
+                    Olhos = f.Olhos,
+                    Preco = f.Preco,
+                    Descricao = f.Descricao
+                });
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ Capitu.Service/IFornecedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Capitu.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFornecedor" in both code and config file together.
+    [ServiceContract]
+    public interface IFornecedor
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        [Description("Returns a copy of the restaurant menu.")]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare,
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)]
+        [AspNetCacheProfile("CacheFor10Seconds")]
+        List<FornecedorDTO> GetFornecedores();
+    }
+}
